9e509e58fd107de107df05f2559db963679b9f68
Himank101 <b.himank101@gmail.com>
2024-07-09 10:31:18 +0530

    woops

--- test1/implementation/solution.py
+++
@@ -1,7 +0,0 @@
-# Placeholder implementation
-
-def main():
-    print('Hello, this is a placeholder implementation.')
-
-if __name__ == '__main__':
-    main()
--- test1/tools/read_dirs.py
+++
@@ -1,8 +0,0 @@
-from langchain.tools import tool
-import os
-
-class DirReadTool():
-    @tool("Create directory")
-    def dir_read_tool(directory_path):
-        """Useful to list files and directories with the given path."""
-        return os.listdir()
